aabedd1ed7d846ec229f29ca12ca2208e9c1a57a
Manuel Avalos <manuelavalos7@yahoo.com>
2020-10-17 18:59:25 -0500

    making request to backend from react, not rendering yet

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,18 @@
 import React from 'react';
 
 const home = () => {
+    var result;
+    fetch('http://18.224.61.179:5000/')
+    .then(res=> res.text())
+    .then(text=> {
+        console.log('parsed: ', text);
+        result = text;
+    })
+    
     return (
        <div>
           <h1>Home</h1>
-           <p>Home page body content</p>
+    <p>Home page body content {result}</p>
        </div>
     );
 }
