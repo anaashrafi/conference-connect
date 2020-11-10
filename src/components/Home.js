e4734dc64d4a10bd7f6c284678cb24748fd54d0f
Manuel Avalos <manuelavalos7@yahoo.com>
2020-11-10 17:56:26 -0600

    rendering response from backend

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,20 +1,33 @@
 import React from 'react';
 
-const home = () => {
-    var result;
-    fetch('http://18.224.61.179:5000/')
-    .then(res=> res.text())
-    .then(text=> {
-        console.log('parsed: ', text);
-        result = text;
-    })
-    
-    return (
-       <div>
-          <h1>Home</h1>
-    <p>Home page body content {result}</p>
-       </div>
-    );
+class home extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {result: "none"};
+    }
+
+    componentDidMount(){
+        const api = 'http://18.224.61.179:5000/'
+        console.log("component mounted");
+        var result;
+        fetch(api)
+        .then(res=> res.text())
+        .then(text=> {
+            console.log('parsed: ', text);
+            this.setState(  {result : text});
+        })    
+    }
+
+    render(){
+
+        return (
+           <div>
+            <h1>Home</h1>
+            <p>Home page body content {this.state.result}</p>
+           </div>
+        );
+    }
+
 }
  
 export default home;
