38b24a9c6a244065e502e5018e07c41f6369435a
anaashrafi <ana.a.ashrafi@gmail.com>
2020-11-15 16:31:21 -0600

    added home page content for registering band

--- src/App.js
+++ src/App.js
@@ -27,6 +27,7 @@
 
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import './App.css';
  
 import Home from './components/Home';
 import Connections from './components/Connections';
