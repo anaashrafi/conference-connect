bc5487079aea314569bb713443bda0162a22256a
An Nguyen <anxnguyen@utexas.edu>
2020-11-15 14:42:04 -0600

    testing new branch

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -4,7 +4,7 @@ const Profile = () => {
     return (
        <div>
           <h1>Profile</h1>
-           <p>Profile page body content</p>
+           <p>Profile page body content test</p>
        </div>
     );
 }
